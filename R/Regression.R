599881f9c0bb5f5b322a913b0cc71c7eb5a93232
Fatih <fatih.bostanci@hotmail.de>
2019-11-27 14:39:08 +0100

    working on regression and playing with documentation

--- R/Regression.R
+++ R/Regression.R
@@ -1,14 +1,14 @@
-# % [beta, w] = elemfits(x,y)
-# % elemfits compute the Nx(N-1)/2 elemental fits, i.e., intercepts b_{0,ij}
-# % and slopes b_{1,ij}, that define a line y = b_0+b_1 x that passes through
-# % the data points (x_i,y_i) and (x_j,y_j), i<j, where i, j in {1, ..., N}.
-# % and the respective weights | x_i - x_j |
-#   % INPUT:
-#   %    y : (numeric) N x 1 vector of real-valued outputs (response vector)
-# %    x : (numeric) N x 1 vector of inputs (feature vector)
-# % OUTPUT:
-#   %  beta: (numeric) N*(N-1)/2 matrix of elemental fits
-# %     w: (numeric) N*(N-1)/2 matrix of weights
+#'   [beta, w] = elemfits(x,y)
+#'   elemfits compute the Nx(N-1)/2 elemental fits, i.e., intercepts b_{0,ij}
+#'   and slopes b_{1,ij}, that define a line y = b_0+b_1 x that passes through
+#'   the data points (x_i,y_i) and (x_j,y_j), i<j, where i, j in {1, ..., N}.
+#'   and the respective weights | x_i - x_j |
+#'
+#'    @param   y : (numeric) N x 1 vector of real-valued outputs (response vector)
+#'    @param   x : (numeric) N x 1 vector of inputs (feature vector)
+#'
+#'    @return    beta: (numeric) N*(N-1)/2 matrix of elemental fits
+#'    @return   w: (numeric) N*(N-1)/2 matrix of weights
 #' @export
 elemfits <- function(y, x){
   N <- length(x)
@@ -24,23 +24,25 @@ elemfits <- function(y, x){
   return (list(beta, abs(w)))
 }
 
-#' % enet computes the elastic net estimator using the cyclic co-ordinate
-#' % descent (CCD) algorithm.
-#' %
-#' % INPUT:
-#'   %       y : (numeric) data vector of size N x 1 (output, respones)
-#' %         if the intercept is in the model, then y needs to be centered.
-#' %       X : (numeric) data matrix of size N x p (input, features)
-#' %           Columns are assumed to be standardized (i.e., norm(X(:,j))=1)
-#' %           as well as centered (if intercept is in the model).
-#' %    beta : (numeric) regression vector for initial start for CCD algorithm
-#' %  lambda : (numeric) a postive penalty parameter value
-#' %  alpha  : (numeric) elastic net tuning parameter in the range [0,1]. If
-#' %           not given then use alpha = 1 (Lasso)
-#' % printitn: print iteration number (default = 0, no printing)
-#' % OUTPUT:
-#'   %   beta    : (numberic) the regression coefficient vector
-#' %   iter  : (numeric) # of iterations
+
+
+
+#'   enet computes the elastic net estimator using the cyclic co-ordinate
+#'   descent (CCD) algorithm.
+#'
+#'     @param       y : (numeric) data vector of size N x 1 (output, respones)
+#'           if the intercept is in the model, then y needs to be centered.
+#'     @param    X : (numeric) data matrix of size N x p (input, features)
+#'             Columns are assumed to be standardized (i.e., norm(X(:,j))=1)
+#'             as well as centered (if intercept is in the model).
+#'   @param   beta : (numeric) regression vector for initial start for CCD algorithm
+#'   @param lambda : (numeric) a postive penalty parameter value
+#'   @param alpha  : (numeric) elastic net tuning parameter in the range [0,1]. If
+#'             not given then use alpha = 1 (Lasso)
+#'  @param printitn: print iteration number (default = 0, no printing)
+#'
+#'   @return     beta    : (numberic) the regression coefficient vector
+#'  @return iter  : (numeric) # of iterations
 #' @export
 enet <- function(y, X, beta, lambda, alpha=1, printitn=0, itermax = 1000){
   # check for valid arguments
@@ -74,7 +76,7 @@ enet <- function(y, X, beta, lambda, alpha=1, printitn=0, itermax = 1000){
       sprintf('enet: %4d  crit = %.8f\n',iter,crit)
     }
 
-    if(crit < 1e-4) {      break }
+    if(is.nan(crit) | crit < 1e-4) {      break }
 
     betaold <- beta
     normb0 <- normb
@@ -83,32 +85,34 @@ enet <- function(y, X, beta, lambda, alpha=1, printitn=0, itermax = 1000){
   return (list(beta, iter))
 }
 
+
+
 #'  [B, lamgrid, BIC, MSE] = enetpath(y, X,...)
 #'  enethpath computes the elastic net (EN) regularization path (over grid
 #' of penalty parameter values). Uses pathwise CCD algorithm.
-#'  INPUT:
-#'        y : Numeric data vector of size N x 1 (output, respones)
-#'       X : Nnumeric data matrix of size N x p. Each row represents one
+#'
+#'  @param       y : Numeric data vector of size N x 1 (output, respones)
+#'  @param     X : Nnumeric data matrix of size N x p. Each row represents one
 #'          observation, and each column represents one predictor (feature).
-#'   intcpt: Logical flag to indicate if intercept is in the model
-#'   alpha  : Numeric scalar, elastic net tuning parameter in the range [0,1].
+#'  @param intcpt: Logical flag to indicate if intercept is in the model
+#'  @param alpha  : Numeric scalar, elastic net tuning parameter in the range [0,1].
 #'            If not given then use alpha = 1 (Lasso)
-#'       eps: Positive scalar, the ratio of the smallest to the
+#'   @param    eps: Positive scalar, the ratio of the smallest to the
 #'           largest Lambda value in the grid. Default is eps = 10^-4.
-#'        L : Positive integer, the number of lambda values EN/Lasso uses.
+#'   @param     L : Positive integer, the number of lambda values EN/Lasso uses.
 #'            Default is L=100.
-#'  printitn: print iteration number (default = 0, no printing)
-#'  OUTPUT:
-#'      B    : Fitted EN/Lasso regression coefficients, a p-by-(L+1) matrix,
+#'  @param printitn: print iteration number (default = 0, no printing)
+#'
+#'  @return    B    : Fitted EN/Lasso regression coefficients, a p-by-(L+1) matrix,
 #'           where p is the number of predictors (columns) in X, and L is
 #'           the  number of Lambda values. If intercept is in the model, then
 #'           B is (p+1)-by-(L+1) matrix, with first element the intercept.
-#'   stats  : structure with following fields:
+#'  @return stats  : structure with following fields:
 #'              Lambda = lambda parameters in ascending order
 #'            MSE = Mean squared error (MSE)
 #'            BIC = Bayesian information criterion values for each lambda
 #' @export
-enetpath <- function(y, X, intcpt=TRUE, alpha=1, eps=1e-4, L=100, printitn=0){
+enetpath <- function(y, X, alpha=1,  L=100, eps=1e-4, intcpt=TRUE, printitn=0){
   # TODO check for valid arguments
 
   n <- nrow(X)
@@ -136,7 +140,7 @@ enetpath <- function(y, X, intcpt=TRUE, alpha=1, eps=1e-4, L=100, printitn=0){
   B <- matrix(0,p,L+1)
 
   for(jj in 1:L){
-    B[,jj+1] <- enet(y, X, B[,jj], lamgrid[jj+1], alpha, printitn)
+    B[,jj+1] <- enet(y, X, B[,jj], lamgrid[jj+1], alpha, printitn)[[1]]
   }
 
   B[abs(B)<5e-8] <- 0
@@ -144,7 +148,7 @@ enetpath <- function(y, X, intcpt=TRUE, alpha=1, eps=1e-4, L=100, printitn=0){
   DF = colSums(abs(B) != 0)
 
   if(n>p){
-      MSE <- colSums(y - abs(X %*% B)^2) * (1 / (n - DF - 1))
+      MSE <- colSums(abs(y - X %*% B)^2) * (1 / (n - DF - 1))
       BIC <- n * log(MSE) + DF * log(n)
   } else
   {
@@ -154,10 +158,221 @@ enetpath <- function(y, X, intcpt=TRUE, alpha=1, eps=1e-4, L=100, printitn=0){
 
 
   B <- B / sdX
-  if(intcpt) B <- cbind(meany - meanX %*% B, B)
 
-  stats <- c(MSE, BIC, lamgrid)
-  names(stats) <- c('MSE', 'BIC', 'Lambda')
+  if(intcpt) B <- rbind(meany - meanX %*% B, B)
+
+  stats <- matrix(c(MSE, BIC, lamgrid), nrow = L+1, ncol = 3, dimnames = list(NULL ,c('MSE', 'BIC', 'Lambda')))
 
   return (list(B, stats))
+}
+
+
+
+#'   hublasso computes the M-Lasso estimate for a given penalty parameter
+#'   using Huber's loss function
+#'
+#'   @param       y: Numeric data vector of size N x 1 (output,respones)
+#'   @param       X: Numeric data matrix of size N x p (inputs,predictors,features).
+#'             Each row represents one observation, and each column represents
+#'             one predictor
+#'
+#'   @param  lambda: positive penalty parameter value
+#'   @param      b0: numeric initial start of the regression vector
+#'   @param    sig0: numeric positive scalar, initial scale estimate.
+#'   @param  c: Threshold constant of Huber's loss function
+#'   @param  reltol: Convergence threshold. Terminate when successive
+#'             estimates differ in L2 norm by a rel. amount less than reltol.
+#'             Default is 1.0e-5
+#'   @param  iter_max: int, default = 500. maximum number of iterations
+#'   @param  printitn: print iteration number (default = 0, no printing)
+#'
+#'   @return        b0: regression coefficient vector estimate
+#'   @return     sig0: estimate of the scale
+#'   @return   psires: pseudoresiduals
+#' @export
+hublasso <- function(y, X, c = NULL, lambda, b0,  sig0, reltol = 1e-5, printitn = 0, iter_max = 500){
+  n <- nrow(X)
+  p <- ncol(X)
+
+  complex_data <- is.complex(X)
+
+  if(is.null(c)){if(complex_data) c <- 1.1214 else c <- 1.345}
+
+  csq <- c^2
+
+  # consistency factor for scale
+  if(complex_data){
+    qn <- pchisq(2 * csq, 2)
+    alpha <- pchisq(2 * csq, 4) + csq * (1 - qn)
+  } else{
+    qn <- pchisq(csq, 1)
+    alpha <- pchisq(csq, 3) + csq * (1 - qn)
+  }
+
+  con <- sqrt(n * alpha)
+  betaold <- b0
+  normb0 <- norm(matrix(b0), type = "2")
+
+  for(iter in 1:iter_max){
+    r <- y - X %*% b0
+    psires <- psihub(r/sig0, c) * sig0
+    # scale update
+    sig1 <- norm(matrix(psires), type = "2") / con
+
+    crit2 <- abs(sig0 - sig1)
+    for(jj in 1:p){
+      # update pseudoresidual
+      psires <- psihub(r/sig1, c) * sig1
+      b0[jj] <- soft_thresh(b0[jj] + X[,jj] %*% psires, lambda)
+      r <- r + X[,jj] * (betaold[jj] - b0[jj])
+    }
+
+    normb <- norm(matrix(b0), type = "2")
+
+    crit <- sqrt(normb0^2 + normb^2 - 2*Re(betaold %*% b0)) / normb0
+
+    if(printitn > 0){
+      r <- (y - X %*% b0) / sig1
+      objnew <- sig1 * colSums(rhofun(r, c)) + (beta / 2) * n * sig1 + lambda * sum(abs(b0))
+      sprinf('Iter %3d obj =  %10.5f crit = %10.5f crit2 = %10.5f\n',
+             iter,objnew,crit,crit2)
+    }
+
+    if(is.na(crit) | crit < reltol){ break }
+
+    sig0 <- sig1
+    betaold <- b0
+    normb0 <- normb
+  }
+
+  if(printitn > 0){
+    # Check the M-Lasso estimating equations
+    b0[abs(b0) < 5e-9] <- 0
+    r <- y - X * b0
+    s <- sign(b0)
+    ind <- 1:p
+    ind2 <- ind[s==0]
+    psires <- psihub(r / sig1, c) * sig1
+    s[ind2] <- X[, ind2] * psires / lambda
+    # FP equation equal to zero
+    fpeq <- -X * psires + lambda * s
+    sprintf('lam = %.4f it = %d norm(FPeq1)= %.12f abs(FPeq2)=%.12f\n',
+            lambda,iter, norm(fpeq),sig1-norm(psires)/con)
+  }
+
+  return (list(b0, sig0, psires))
+}
+
+
+
+#'   hublassopath computes the M-Lasso regularization path (over grid
+#'                                                            of penalty parameter values) using Huber's loss function
+#'
+#'@param          y: Numeric data vector of size N x 1 (output, respones)
+#'@param          X: Numeric data matrix of size N x p. Each row represents one
+#'             observation, and each column represents one predictor (feature)
+#'             columns are standardized to unit length.
+#'@param          c: Threshold constant of Huber's loss function (optional;
+#'                                                                       otherwise use default value)
+#'@param       intcpt: Logical (true/false) flag to indicate if intercept is in the
+#'             regression mode. Default is true.
+#'@param        eps: Positive scalar, the ratio of the smallest to the
+#'             largest Lambda value in the grid. Default is eps = 10^-3.
+#'@param         L : Positive integer, the number of lambda values EN/Lasso uses.
+#'             Default is L=120.
+#'@param     reltol : Convergence threshold for IRWLS. Terminate when successive
+#'             estimates differ in L2 norm by a rel. amount less than reltol. default: 1e-05
+#'@param   printitn: print iteration number (default = 0, no printing)
+#'
+#'@return       B    : Fitted M-Lasso regression coefficients, a p-by-(L+1) matrix,
+#'            where p is the number of predictors (columns) in X, and L is
+#'            the  number of Lambda values.
+#'@return       B0 : estimates values of intercepts
+#'@return   stats  : structure with following fields:
+#'\itemize{
+#'               \item Lambda = lambda parameters in ascending order
+#'             \item sigma = estimates of the scale (a (L+1) x 1 vector)
+#'             \item gBIC = generalized Bayesian information criterion (gBIC) value
+#'                   for each lambda parameter on the grid.}
+#'@export
+hublassopath <- function(y, X, c = NULL, L = 120, eps =10^-3, intcpt = T, reltol = 1e-5, printitn = 0){
+  n <- nrow(X)
+  p <- ncol(X)
+
+  if(is.complex(y)) real_data <- F else real_data <- T
+  # Default: approx 95 efficiency for Gaussian errors
+  if(is.null(c)){if(real_data) c <- 1.3414 else c <- 1.215}
+
+  # tmp <- hubreg(y, rep(1, n), c)
+  # TODO continue when done with hubreg
+}
+
+
+
+#'   hubreg computes the joint M-estimates of regression and scale using
+#'   Huber's criterion. Function works for both real- and complex-valued data.
+#'
+#'
+#'@param   y: Numeric data vector of size N x 1 (output, respones)
+#'@param   X: Numeric data matrix of size N x p. Each row represents one
+#'             observation, and each column represents one predictor (feature).
+#'             If the model has an intercept, then first column needs to be a
+#'             vector of ones.
+#'@param    c: numeric threshold constant of Huber's function
+#'@param    sig0: (numeric) initial estimator of scale [default: SQRT(1/(n-p)*RSS)]
+#'@param    b0: initial estimator of regression (default: LSE)
+#'@param    printitn: print iteration number (default = 0, no printing)
+#'@param    iter_max: maximum number of iterations. \cr default = 2000
+#'@param    errortol: ERROR TOLERANCE FOR HALTING CRITERION. \cr default = 1e-5
+#'
+#'@return    b1: the regression coefficient vector estimate
+#'@return    sig1: the estimate of scale
+#'@return    iter: the # of iterations
+#'@export
+hubreg <- function(y, X, c = NULL, sig0 = NULL, b0 = NULL, printitn = 0, iter_max = 2000, errortol = 1e-5){
+  n <- nrow(X)
+  p <- ncol(X)
+
+  if(is.complex(y)) real_data <- F else real_data <- T
+  # Default: approx 95 efficiency for Gaussian errors
+  if(is.null(c)){if(real_data) c <- 1.3415 else c <- 1.215}
+
+  if(is.null(b0)) b0 <- solve(X, y)
+
+  # use unbiased residual sum of squares as initial estimate of scale
+  if(is.null(sig0)){sig0 <- norm(matrix(y - X %*% b0), type = "2") /
+    sqrt(n - p)}
+
+  csq <- c^2
+
+  # consistency factor for scale
+  if(complex_data){
+    qn <- pchisq(2 * csq, 2)
+    alpha <- pchisq(2 * csq, 4) + csq * (1 - qn)
+  } else{
+    qn <- pchisq(csq, 1)
+    alpha <- pchisq(csq, 3) + csq * (1 - qn)
+  }
+
+  Z <- pinv(X)
+
+  con <- sqrt((n - p) * alpha)
+
+  for(iter in 1:iter_max){
+    # Step 1: Update residual
+    r <- y - X %*% b0
+    psires <- psihub(r / sig0, c) * sig0
+
+    # Step 2: Update the scale
+    sig1 <- norm(matrix(psires), type ="2") / con
+
+    # Step 3: Update the pseudo-residual
+    psires <- psihub(r / sig1, c) * sig1
+
+    # Step 4: regress X on pseudo-residual
+    update <- Z * psires
+
+    # Step 5: update the beta
+    b1 <- b0 + update
+  }
 }
--- tests/testthat/test_reg.R
+++ tests/testthat/test_reg.R
@@ -4,4 +4,21 @@ test_that('reg stuff', {
   y <- 1:4
   X <- matrix(c(0,1,0,0,1,0,0,0),4,2)
   expect_equal(enet(1:2, matrix(c(0, 1, 1, 0), 2, 2), c(-.1,.4),0.5), list(c(1.5, .5), 2))
+
+})
+
+test_that('hublasso',{
+  y <- c(1.3, 4, 5)
+  X <- matrix(c(4,5,6,7,8,9,0,3,0), nrow = 3, ncol = 3, byrow = T)
+  lambda <- 3.3
+  b0 <- c(0,0,0)
+  c <- 4
+  sig0 <- 1
+  reltol <- 1e-5
+  printitn <- 0
+  iter_max <- 500
+  expect_equal(hublasso(y, X, lambda, c, sig0, reltol, printitn),
+               list(
+
+               ))
 })
